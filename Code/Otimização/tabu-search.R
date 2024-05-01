dd3431a8e37940591df91a1a5d5aa5e28a8e9cb9
simao481 <simao.daniel.silva.dias@gmail.com>
2024-05-01 22:58:56 +0100

    Tabu-search finished

--- Code/Otimização/tabu-search.R
+++ Code/Otimização/tabu-search.R
@@ -6,6 +6,7 @@ library(adana)
 
 source("C:/Users/Neon/Documents/Universidade/4º Ano/TIAPOSE/Walmart-TIAPOSE/Code/OTIMIZATION/Functions_Otimization.R")
 
+# Function to divide binary array by bits 
 matrix_transform <- function(solution, start, elements, dimension_start, bits){
   matrix_final <- c()
   for(i in 1:elements){
@@ -15,6 +16,7 @@ matrix_transform <- function(solution, start, elements, dimension_start, bits){
   return(matrix_final)
 }
 
+# Evaluation Function
 eval <- function(solution){
   hired_workers  <- matrix(matrix_transform(solution        = solution, 
                                             start           = 1, 
@@ -34,6 +36,46 @@ eval <- function(solution){
   return(monthly_profit)
 }
 
+# Evaluation Function 
+F2 <- function(solution){
+  hired_workers  <- matrix(matrix_transform(solution        = solution, 
+                                            start           = 1, 
+                                            elements        = 12,
+                                            dimension_start = 1,
+                                            bits            = bits_workers), nrow = 3, ncol = 4)
+  
+  product_orders <- matrix(matrix_transform(solution        = solution, 
+                                            start           = 12 * bits_workers + 1, 
+                                            elements        = 16, 
+                                            dimension_start = 13, 
+                                            bits            = bits_orders), nrow = 4, ncol = 4)
+  
+  monthly_effort <- total_number_of_workers(hired_workers) + total_number_of_orders(product_orders)
+  return(-monthly_effort) # needs to be negative because tabuSearch only maximizes
+}
+
+# Evaluation Function Multi Objective
+multi_eval <- function(solution){
+  hired_workers  <- matrix(matrix_transform(solution        = solution, 
+                                            start           = 1, 
+                                            elements        = 12,
+                                            dimension_start = 1,
+                                            bits            = bits_workers), nrow = 3, ncol = 4)
+  
+  product_orders <- matrix(matrix_transform(solution        = solution, 
+                                            start           = 12 * bits_workers + 1, 
+                                            elements        = 16, 
+                                            dimension_start = 13, 
+                                            bits            = bits_orders), nrow = 4, ncol = 4)
+  
+  monthly_effort <- total_number_of_workers(hired_workers) + total_number_of_orders(product_orders)
+  sales          <- calculate_sales(actual_sales, hired_workers, product_orders)
+  monthly_profit <- sales_in_usd(sales) - total_costs(hired_workers, product_orders, sales)
+  
+  return(monthly_profit-monthly_effort)
+}
+
+# Function to build Initial Config
 initial_config_build <- function(config, n_bits, dimensions){
   initial_length <- length(config)
   while(length(config) - initial_length < dimensions * n_bits){
@@ -41,6 +83,42 @@ initial_config_build <- function(config, n_bits, dimensions){
   }
   return(config)
 }
+
+tabu_search <- function(initial_config, func){
+  solution <- tabuSearch(size, iters = N, objFunc = func, config = initial_config, verbose = TRUE)
+  
+  b  <- which.max(solution$eUtilityKeep) # best index
+  bs <- solution$configKeep[b,]
+  
+  hired_workers  <- matrix(matrix_transform(solution        = bs,
+                                            start           = 1, 
+                                            elements        = 12,
+                                            dimension_start = 1,
+                                            bits            = bits_workers), nrow = 3, ncol = 4)
+  
+  product_orders <- matrix(matrix_transform(solution        = bs, 
+                                            start           = 12 * bits_workers + 1, 
+                                            elements        = 16, 
+                                            dimension_start = 13,
+                                            bits            = bits_orders), nrow = 4, ncol = 4)
+  
+  
+  monthly_effort <- total_number_of_workers(hired_workers) + total_number_of_orders(product_orders)
+  sales          <- calculate_sales(actual_sales, hired_workers, product_orders)
+  monthly_profit <- sales_in_usd(sales) - total_costs(hired_workers, product_orders, sales)
+  
+  cat("Best Solution: \nHired Workers \n")
+  print(hired_workers) 
+  cat("\nProduct Orders \n")
+  print(product_orders)
+  cat("\nSales \n")
+  print(sales)
+  cat("\nMonthly Profit:", monthly_profit,"\n")
+  cat("\nMonthly Effort:", monthly_effort,"\n")
+  cat("\nSolution in Binary Space\n")
+  print(bs)
+}
+#-------------------------------------------------------------------
 
 actual_sales <- data.frame(
   WSdep1 = c(54480 , 42221 , 36267 , 35283 ),
@@ -55,7 +133,7 @@ Low          <- rep(0, 28) # Lower
 Up           <- calculate_uppers(actual_sales) # Upper
 bits_workers <- ceiling(max(log(Up[1:12] , 2))) # Bits for Hired Workers
 bits_orders  <- ceiling(max(log(Up[13:28], 2))) # Bits for Product Orders
-N            <- 1000 # number of iterations
+N            <- 100 # number of iterations
 size         <- 12 * bits_workers + 16 * bits_orders # solution size
 
 cat("Tabu Search Sphere D =", D,"( iters =", N,")\n")
@@ -74,32 +152,9 @@ s <- initial_config_build(config     = s,
                           dimensions = 16)
 
 # Do the Tabu Search
-s <- tabuSearch(size, iters = N, objFunc = eval, config = s, verbose = TRUE)
-
-b  <- which.max(s$eUtilityKeep) # best index
-bs <- s$configKeep[b,]
-
-hired_workers  <- matrix(matrix_transform(solution        = bs,
-                                          start           = 1, 
-                                          elements        = 12,
-                                          dimension_start = 1,
-                                          bits            = bits_workers), nrow = 3, ncol = 4)
-
-product_orders <- matrix(matrix_transform(solution        = bs, 
-                                          start           = 12 * bits_workers + 1, 
-                                          elements        = 16, 
-                                          dimension_start = 13,
-                                          bits            = bits_orders), nrow = 4, ncol = 4)
-
-sales          <- calculate_sales(actual_sales, hired_workers, product_orders)
-monthly_profit <- sales_in_usd(sales) - total_costs(hired_workers, product_orders, sales)
-
-cat("Best Solution: \nHired Workers \n")
-print(hired_workers) 
-cat("\nProduct Orders \n")
-print(product_orders) 
-cat("\nSales \n")
-print(sales)
-cat("\nMonthly Profit:", monthly_profit,"\n")
-cat("\nSolution in Binary Space\n")
-print(bs)
+cat("### MULTI ####\n")
+tabu_search(s, multi_eval)
+cat("\n\n### Eval ####\n")
+tabu_search(s, eval)
+cat("\n\n### F2 ####\n")
+tabu_search(s, F2)
